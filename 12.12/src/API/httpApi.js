7988fbc7e85fba175ee570a3815c75f049d791ad
hjq <1356709443@qq.com>
2022-12-12 22:27:19 +0800

    2201B侯建荞

---
+++ 12.12/src/API/httpApi.js
@@ -0,0 +1,61 @@
+import http from '@/untils/http'
+/**获取用户列表信息 */
+export const GetUser = (params) => {
+	return http.request("/users", params).then(res => {
+		return res
+	})
+}
+/**搜索接口 */
+export const GetSearchUser = (params) => {
+	return http.request("/users", params).then(res => {
+		return res
+	})
+}
+/**添加用户 */
+export const SetUser = (data) => {
+	return http.request("/users",data, 'post').then(res => {
+		return res
+	})
+}
+/**编辑用户 */
+export const UpdateUser = (data) => {
+	return http.request("/users",data, 'put').then(res => {
+		return res
+	})
+}
+/**删除用户 */
+export const DelUser = (id) => {
+	return http.request("/users",id, 'delete').then(res => {
+		return res
+	})
+}
+/**修改用户角色 */
+export const UpdateUserAss = (obj) => {
+	return http.request("/users",obj, 'put').then(res => {
+		return res
+	})
+}
+/**获取角色列表 */
+export const GetRoles = () => {
+	return http.request("/roles",'get').then(res => {
+		return res
+	})
+}
+/**添加角色 */
+export const AddRoles = (data) => {
+	return http.request("/roles",data,'post').then(res => {
+		return res
+	})
+}
+/**删除角色 */
+export const DelRoles = (id) => {
+	return http.request("/roles",id,'delete').then(res => {
+		return res
+	})
+}
+/**编辑角色 */
+export const EditRoles = (data) => {
+	return http.request("/roles",data,'put').then(res => {
+		return res
+	})
+}
---
+++ 12.12/src/views/AuthorityManagement/TreeRoles.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+		<el-dialog
+      title="分配权限"
+      :visible.sync="showTree"
+      width="40%">
+      <el-tree
+        ref="tree"
+        :data="trees"
+        show-checkbox
+        node-key="id"
+        :props="propsObj"
+        :default-expand-all="true">
+      </el-tree>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="showTree = false">取 消</el-button>
+        <el-button type="primary" @click="getCheckedKeys">确 定</el-button>
+      </span>
+    </el-dialog>
+ </div>
+</template>
+
+<script>
+export default {
+  data () {
+	return {
+		showTree: false,
+      trees: [],
+      propsObj: {
+        label: 'authName'
+      },
+      rightsId: []
+
+	}
+  },
+  methods: {
+
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  components: {
+
+  },
+  computed: {
+
+  },
+  watch: {
+
+  },
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
