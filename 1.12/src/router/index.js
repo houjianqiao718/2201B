894a1fac4aadbe1103020d8f708ec08a62dae461
hjq <1356709443@qq.com>
2023-01-12 23:10:57 +0800

    2201B侯建荞

---
+++ 1.12/src/router/index.js
@@ -0,0 +1,96 @@
+import Vue from "vue"
+import VueRouter from "vue-router"
+import HomeView from "../views/Common/HomeView.vue"
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () => import("../views/Common/AboutView.vue"),
+    children: [
+      {
+        path: "/userIndex",
+        name: "userIndex",
+        component: () => import("../views/One/userIndex.vue")
+      },
+      {
+        path: "/Myinfor",
+        name: "Myinfor",
+        component: () => import("../views/One/Myinfor.vue")
+      },
+      {
+        path: "/StaffIn",
+        name: "StaffIn",
+        component: () => import("../views/Two/StaffIn.vue")
+      },
+      {
+        path: "/Staffsee",
+        name: "Staffsee",
+        component: () => import("../views/Two/Staffsee.vue")
+      },
+      {
+        path: "/StaffImport",
+        name: "StaffImport",
+        component: () => import("../views/Two/StaffImport.vue")
+      },
+      {
+        path: "/Setup",
+        name: "Setup",
+        component: () => import("../views/Three/Setup.vue")
+      },
+      {
+        path: "/Permission",
+        name: "Permission",
+        component: () => import("../views/Four/Permission.vue")
+      },
+      {
+        path: "/Security",
+        name: "Security",
+        component: () => import("../views/Five/Security.vue")
+      },
+      {
+        path: "/Sechistory",
+        name: "Sechistory",
+        component: () => import("../views/Five/Sechistory.vue")
+      },
+      {
+        path: "/SecMonreport",
+        name: "SecMonreport",
+        component: () => import("../views/Five/SecMonreport.vue")
+      },
+      {
+        path: "/Secdetail",
+        name: "Secdetail",
+        component: () => import("../views/Five/Secdetail.vue")
+      },
+      {
+        path: "/Approval",
+        name: "Approval",
+        component: () => import("../views/Six/Approval")
+      },
+      {
+        path: "/SecuritySetting",
+        name: "SecuritySetting",
+        component: () => import("../views/Six/SecuritySetting")
+      },
+      {
+        path: "/Appsee",
+        name: "Appsee",
+        component: () => import("../views/Six/Appsee")
+      }
+    ]
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ 1.12/src/views/Six/Apptop.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- 审批头部模块 -->
+  <div class="Apptop">
+    <el-tag size="large"><i class="el-icon-warning"></i> 当前审批中0 本月审批通过1 本月审批驳回0</el-tag>
+    <el-button type="primary" size="mini" @click="$router.push('/SecuritySetting')">流程设置</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.Apptop {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #fff;
+  margin: 10px;
+  padding: 15px;
+  box-sizing: border-box;
+}
+</style>
